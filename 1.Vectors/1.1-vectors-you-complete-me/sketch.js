f643996b917af1055c5402042344d8ca53511aef
JustinBrown <brown.jt@gmail.com>
2016-06-25 13:37:54 -0600

    Example 1.1 finished.

---
+++ 1.Vectors/1.1-vectors-you-complete-me/sketch.js
@@ -0,0 +1,32 @@
+var x = 100;
+var y = 100;
+var xspeed = 6;
+var yspeed = 4.9;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  background(255);
+  x = x + xspeed;
+  y = y + yspeed;
+
+  if ((x > width) || (x < 0)) {
+    xspeed = xspeed * -1;
+  }
+  if ((y > height) || (y < 0)) {
+    yspeed = yspeed * -1;
+  }
+
+  fill(70);
+  ellipse(x,y,100,100);
+  // ellipse(50, 50, 80, 80);
+  // if (mouseIsPressed) {
+  //   fill(0);
+  // } else {
+  //   fill(255);
+  // }
+  // ellipse(mouseX, mouseY, 80, 80);
+}
